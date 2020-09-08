991a26b9a67add986dbe1fae1eed407cb1fdaad3
ronngo <ronngomn23@gmail.com>
2020-09-09 06:43:50 +0700

    first commit

---
+++ ptbacnhat/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+
+namespace ptbacnhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Nhập vào a: ");
+            float a = float.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+                Console.WriteLine("a phải khác 0");
+            }
+            else
+            {
+                Console.WriteLine("Nhập vào b: ");
+                float b = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("\n");
+                float x = -b / a;
+                Console.WriteLine("{0}x + {1} = 0 \n=> x = {2}", a, b, x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
